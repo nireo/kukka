cce70b3bdd55f7d7331ec5e231552194246ee0ef
nireo <eemil.lehtikangas1@gmail.com>
2025-08-27 23:48:58 +0300

    reworked the structure added basic string parsing and char parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,57 @@
-use std::fmt;
+/// input just contains a reference to a string. this is done to prevent copying.
+pub type Input<'a> = &'a str;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Input<'a> {
-    data: &'a str,
-    pos: usize,
+/// a parser returns a new reference to the string that the next parsers should parse from the
+/// result also contains some data given by a parser if it was successful. Other than that, we
+/// return a basic static string error.
+///
+/// TODO: implement proper errors
+pub type ParseResult<'a, T> = Result<(Input<'a>, T), &'static str>;
+
+pub trait Parser<'a, Out> {
+    fn parse(&self, input: Input<'a>) -> ParseResult<'a, Out>;
 }
 
-impl<'a> Input<'a> {
-    pub fn new(data: &'a str) -> Self {
-        Self { data, pos: 0 }
+impl<'a, F, Out> Parser<'a, Out> for F
+where
+    F: Fn(Input<'a>) -> ParseResult<'a, Out>,
+{
+    fn parse(&self, input: Input<'a>) -> ParseResult<'a, Out> {
+        self(input)
     }
+}
 
-    // this way we have immutability. this also comes in handy when we don't have to worry about
-    // many &mut references all around.
-    pub fn advance(&self, n: usize) -> Self {
-        Self {
-            data: self.data,
-            pos: self.pos + n,
+fn char_parser(expected: char) -> impl Fn(&str) -> ParseResult<char> {
+    move |input: &str| {
+        let mut chars = input.char_indices();
+        match chars.next() {
+            Some((_, c)) if c == expected => {
+                let next_pos = chars.next().map(|(pos, _)| pos).unwrap_or(input.len());
+                Ok((&input[next_pos..], c))
+            }
+            _ => Err("char mismatch"),
         }
     }
+}
 
-    pub fn remaining(&self) -> &'a str {
-        &self.data[self.pos..]
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.pos >= self.data.len()
+fn string_parser(expected: &'static str) -> impl Fn(&str) -> ParseResult<&str> {
+    move |input: &str| {
+        if input.starts_with(expected) {
+            Ok((&input[expected.len()..], &input[..expected.len()]))
+        } else {
+            Err("string mismatch")
+        }
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct ParseError {
-    pub message: String,
-    pub pos: usize,
-}
-
-pub type ParseResult<'a, T> = Result<(T, Input<'a>), ParseError>;
-
-impl fmt::Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "parse error at pos {}: {}", self.pos, self.message)
+fn seq<P1, P2, O1, O2>(p1: P1, p2: P2) -> impl Fn(&str) -> ParseResult<(O1, O2)>
+where
+    P1: Fn(&str) -> ParseResult<O1>,
+    P2: Fn(&str) -> ParseResult<O2>,
+{
+    move |input: &str| {
+        let (rest, r1) = p1(input)?;
+        let (rest, r2) = p2(rest)?;
+        Ok((rest, (r1, r2)))
     }
 }
